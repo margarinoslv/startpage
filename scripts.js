c1d9beadc22ba04b0fd38a56afc619cbe1881520
margarinoslav <79981890+margarinoslv@users.noreply.github.com>
2023-09-09 23:13:26 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,95 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+
+/**
+ * Search function
+ */
+
+const searchInput = document.querySelector("#searchbar > input")
+const searchButton = document.querySelector("#searchbar > button")
+
+const lookup = {"/":"/","deepl":"https://deepl.com/","reddit":"https://reddit.com/","maps":"https://maps.google.com/"}
+const engine = "duckduckgo"
+const engineUrls = {
+  deepl: "https://www.deepl.com/translator#-/-/{query}",
+  duckduckgo: "https://duckduckgo.com/?q={query}",
+  ecosia: "https://www.ecosia.org/search?q={query}",
+  google: "https://www.google.com/search?q={query}",
+  startpage: "https://www.startpage.com/search?q={query}",
+  youtube: "https://www.youtube.com/results?q={query}",
+}
+
+const isWebUrl = value => {
+  try {
+    const url = new URL(value)
+    return url.protocol === "http:" || url.protocol === "https:"
+  } catch {
+    return false
+  }
+}
+
+const getTargetUrl = value => {
+  if (isWebUrl(value)) return value
+  if (lookup[value]) return lookup[value]
+  const url = engineUrls[engine] ?? engine
+  return url.replace("{query}", value)
+}
+
+const search = () => {
+  const value = searchInput.value
+  const targetUrl = getTargetUrl(value)
+  window.open(targetUrl, "_self")
+}
+
+searchInput.onkeyup = event => event.key === "Enter" && search()
+searchButton.onclick = search
+
+/**
+ * inject bookmarks into html
+ */
+
+const bookmarks = [{"id":"Zq9x2z0fHyOtF8lL","label":"reddit","bookmarks":[{"id":"PbG6HuWNdgWZGLDg","label":"reddit","url":"https://www.reddit.com/r/unixporn/"},{"id":"yBbWF2lal847OOF0","label":"discord","url":"discord.com"},{"id":"GWjESRx9ioOFEePy","label":"youtube","url":"youtube.com"}]},{"id":"eL7wN11t9ZYv22ak","label":"work","bookmarks":[{"id":"w91ZmkVbaz5lzThK","label":"github","url":"github.com"},{"id":"qaIq0mwx1kQ8kvxn","label":"freecodecamp","url":"https://www.freecodecamp.org/"}]},{"id":"XT3W817p2B6jrt4z","label":"games","bookmarks":[{"id":"H7e1VcoVFwbv2FCg","label":"chess","url":"chess.com"},{"id":"ZU4eU1ZW39vrE7D2","label":"monkeytype","url":"monkeytype.com"},{"id":"HCsadFV7GVfs3ean","label":"tetris","url":"tetr.io"}]}]
+
+const createGroupContainer = () => {
+  const container = document.createElement("div")
+  container.className = "bookmark-group"
+  return container
+}
+
+const createGroupTitle = title => {
+  const h2 = document.createElement("h2")
+  h2.innerHTML = title
+  return h2
+}
+
+const createBookmark = ({ label, url }) => {
+  const li = document.createElement("li")
+  const a = document.createElement("a")
+  a.href = url
+  a.innerHTML = label
+  li.append(a)
+  return li
+}
+
+const createBookmarkList = bookmarks => {
+  const ul = document.createElement("ul")
+  bookmarks.map(createBookmark).forEach(li => ul.append(li))
+  return ul
+}
+
+const createGroup = ({ label, bookmarks }) => {
+  const container = createGroupContainer()
+  const title = createGroupTitle(label)
+  const bookmarkList = createBookmarkList(bookmarks)
+  container.append(title)
+  container.append(bookmarkList)
+  return container
+}
+
+const injectBookmarks = () => {
+  const bookmarksContainer = document.getElementById("bookmarks")
+  bookmarksContainer.append()
+  bookmarks.map(createGroup).forEach(group => bookmarksContainer.append(group))
+}
+
+injectBookmarks()
